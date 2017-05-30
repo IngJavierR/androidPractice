35e9e0f1b54d714130e000645335832113e04a7c
IngJavierR <hazelapd@gmail.com>
2017-05-29 23:40:49 -0500

    Fragments

--- app/src/main/java/com/example/home/androidpractice/HomeActivity.java
+++ app/src/main/java/com/example/home/androidpractice/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.home.androidpractice;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        HelloFragment helloFragment = new HelloFragment();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, helloFragment, "helloFragment");
+        transaction.commit();
     }
 }
