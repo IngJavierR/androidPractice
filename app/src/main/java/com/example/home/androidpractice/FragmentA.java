cf1409af8a3c252f77de8f112d404c24c2b45c8c
IngJavierR <hazelapd@gmail.com>
2017-05-30 23:26:43 -0500

    rotate

--- app/src/main/java/com/example/home/androidpractice/FragmentA.java
+++ app/src/main/java/com/example/home/androidpractice/FragmentA.java
@@ -6,16 +6,67 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by jrodriguez on 29/05/17.
  */
 
 public class FragmentA extends Fragment {
+
+    private Button button;
+    private TextView textView;
+    int score;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null){
+            score = savedInstanceState.getInt("score_key", 0);
+        }
+
+        Toast.makeText(getActivity(), "Score value: " + score, Toast.LENGTH_LONG).show();
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+        button = (Button) view.findViewById(R.id.button);
+        textView = (TextView) view.findViewById(R.id.textView);
+
+        if(savedInstanceState != null){
+            textView.setText(savedInstanceState.getString("txt_key"));
+            button.setText(savedInstanceState.getString("btn_key", "LOGIN"));
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                textView.setText("Dummy Text");
+                button.setText("LOGOUT");
+
+                score = 47;
+                Toast.makeText(getActivity(), "Score: " + score, Toast.LENGTH_LONG).show();
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("score_key", score);
+        outState.putString("txt_key", textView.getText().toString());
+        outState.putString("btn_key", button.getText().toString());
     }
 }
--- app/src/main/java/com/example/home/androidpractice/HomeActivity.java
+++ app/src/main/java/com/example/home/androidpractice/HomeActivity.java
@@ -7,9 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity  implements FragmentManager.OnBackStackChangedListener{
+public class HomeActivity extends AppCompatActivity {
 
-    FragmentManager manager;
     private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
@@ -17,146 +16,6 @@ public class HomeActivity extends AppCompatActivity  implements FragmentManager.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        manager = getFragmentManager();
-        manager.addOnBackStackChangedListener(this);
     }
 
-    public void addFragmentA(View view) {
-
-        FragmentA fragmentA = new FragmentA();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, fragmentA, "fragA");
-        transaction.addToBackStack("AddFragA");
-        transaction.commit();
-    }
-
-    public void removeFragmentA(View view) {
-
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentA != null) {
-            transaction.remove(fragmentA);
-            transaction.addToBackStack("RemFragA");
-            transaction.commit();
-        }
-    }
-
-    public void addFragmentB(View view) {
-
-        FragmentB fragmentB = new FragmentB();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, fragmentB, "fragB");
-        transaction.addToBackStack("AddFragB");
-        transaction.commit();
-    }
-
-    public void removeFragmentB(View view) {
-
-        FragmentB fragmentB = (FragmentB) manager.findFragmentByTag("fragB");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentB != null) {
-            transaction.remove(fragmentB);
-            transaction.addToBackStack("RemFragB");
-            transaction.commit();
-        }
-    }
-
-    public void replaceByFragmentA(View view) {
-
-        FragmentA fragmentA = new FragmentA();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, fragmentA, "fragA");
-        transaction.addToBackStack("RepByFragA");
-        transaction.commit();
-    }
-
-    public void replaceByFragmentB(View view) {
-
-        FragmentB fragmentB = new FragmentB();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, fragmentB, "fragB");
-        transaction.addToBackStack("RepByFragB");
-        transaction.commit();
-    }
-
-    public void attachFragmentA(View view) {
-
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentA != null) {
-            transaction.attach(fragmentA);
-            transaction.addToBackStack("AttachFragA");
-            transaction.commit();
-        }
-    }
-
-    public void detachFragmentA(View view) {
-
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentA != null) {
-            transaction.detach(fragmentA);
-            transaction.addToBackStack("DetachFragA");
-            transaction.commit();
-        }
-    }
-
-    public void showFragmentA(View view) {
-
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentA != null) {
-            transaction.show(fragmentA);
-            transaction.addToBackStack("ShowFragA");
-            transaction.commit();
-        }
-    }
-
-    public void hideFragmentA(View view) {
-
-        FragmentA fragmentA = (FragmentA) manager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = manager.beginTransaction();
-        if(fragmentA != null) {
-            transaction.hide(fragmentA);
-            transaction.addToBackStack("HideFragA");
-            transaction.commit();
-        }
-    }
-
-    public void dummyBackButtonClick(View view) {
-        manager.popBackStack();
-    }
-
-    public void pop_AddFragA_Inclusive_Transaction(View view) {
-        manager.popBackStack("AddFragA", FragmentManager.POP_BACK_STACK_INCLUSIVE);
-    }
-
-    public void pop_AddFragB_Transaction(View view) {
-        manager.popBackStack("AddFragB", 0);
-    }
-
-    @Override
-    public void onBackStackChanged() {
-
-        int length = manager.getBackStackEntryCount();
-
-        StringBuilder msg = new StringBuilder("");
-
-        for(int i = length - 1; i >= 0; i--){
-            msg.append("Index ").append(i).append(" : ");
-            msg.append(manager.getBackStackEntryAt(i).getName());
-            msg.append(" \n");
-        }
-        Log.i(TAG, "\n" + msg.toString() + "\n");
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        if(manager.getBackStackEntryCount() > 0){
-            manager.popBackStack();
-        }else{
-            super.onBackPressed();
-        }
-    }
 }
